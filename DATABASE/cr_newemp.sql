30f426f39f954f1bb8d66db7c56c3f5994350d25
SpruhaU <105652937+SpruhaU@users.noreply.github.com>
2022-06-14 08:09:01 +0200

    create employee details sql

--- DATABASE/cr_newemp.sql
+++ DATABASE/cr_newemp.sql
@@ -1,42 +1,37 @@
--- PROCEDURE: public.cr_new_emp(integer, text, character varying, character varying, character varying, character varying, text, character varying, integer, character varying, character varying)
+-- PROCEDURE: public.cr_new_emp(integer, character varying, character varying, character varying, character varying, character varying, date, integer, character varying, integer, integer, integer, integer)
 
--- DROP PROCEDURE IF EXISTS public.cr_new_emp(integer, text, character varying, character varying, character varying, character varying, text, character varying, integer, character varying, character varying);
+-- DROP PROCEDURE IF EXISTS public.cr_new_emp(integer, character varying, character varying, character varying, character varying, character varying, date, integer, character varying, integer, integer, integer, integer);
 
 CREATE OR REPLACE PROCEDURE public.cr_new_emp(
-	IN employee_id integer,
-	IN Job_id int,
-	IN First_Name character varying,
-	IN Middle_Name character varying,
-	IN Last_Name character varying,
+	IN job_id integer,
+	IN first_name character varying,
+	IN middle_name character varying,
+	IN last_name character varying,
 	IN email character varying,
-	IN mobile text,
+	IN mobile character varying,
 	IN date_of_joining date,
-	IN date_of_leaving date,
-	IN Manager_id int
-	IN Gender character varying,
-	IN Accrued_leaves int,
-	IN shift_code int,
-	IN dept_no int,
-	IN emp_type"_id int
-)
+	IN manager_id integer,
+	IN gender character varying,
+	IN accrued_leaves integer,
+	IN shift_code integer,
+	IN dept_no integer,
+	IN emp_type_id integer)
 LANGUAGE 'plpgsql'
 AS $BODY$
 BEGIN
-
-	INSERT into "Employee_table"(
-		    "Employee_ID","Job_ID","First_Name",
-		    "Middle_Name","Last_Name","Email",
-		    "Mobile","Date_of_joining","Date_of_leaving",
-            "Manager_ID","Gender","Accrued_leaves",
-            "Shift_code","Dept_no.","Emp_Type_ID ")
-			
-values (Employee_ID,Job_ID,First_Name,
-		    Middle_Name,Last_Name,Email,
-		    Mobile,Date_of_joining,Date_of_leaving,
-            Manager_ID,Gender,Accrued_leaves,
-            Shift_code,Dept_no.,Emp_Type_ID);
-		
+INSERT into "Employee_table"(
+	"Job_ID","First_Name","Middle_Name",
+    "Last_Name","Email",
+    "Mobile","Date_of_joining",
+	"Manager_ID","Gender",
+	"Accrued_leaves","Shift_code","Dept_no.","Emp_Type_ID ")
+values (
+	Job_ID,First_Name,Middle_Name,
+    Last_Name,Email,
+    Mobile,Date_of_joining,
+	Manager_ID,Gender,
+	Accrued_leaves,Shift_code,Dept_no,Emp_Type_ID);
 end;
 $BODY$;
-ALTER PROCEDURE public.personal_data_emp(integer, text, character varying, character varying, character varying, character varying, text, character varying, integer, character varying, character varying)
+ALTER PROCEDURE public.cr_new_emp(integer, character varying, character varying, character varying, character varying, character varying, date, integer, character varying, integer, integer, integer, integer)
     OWNER TO postgres;
